d888c03d63a0cf5854d8eeff0f9991e26fccaa9a
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-04-18 14:16:26 +0700

    [config] add .cxx/cmake folder

---
+++ TMessagesProj/.cxx/cmake/afatStandalone/x86/CMakeFiles/3.10.2/CMakeASM_NASMCompiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_ASM_NASM_COMPILER "/Users/lambiengcode/Library/Android/sdk/ndk/21.1.6352462/toolchains/llvm/prebuilt/darwin-x86_64/bin/yasm")
+set(CMAKE_ASM_NASM_COMPILER_ARG1 "-DELF")
+set(CMAKE_AR "/Users/lambiengcode/Library/Android/sdk/ndk/21.1.6352462/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android-ar")
+set(CMAKE_ASM_NASM_COMPILER_AR "")
+set(CMAKE_RANLIB "/Users/lambiengcode/Library/Android/sdk/ndk/21.1.6352462/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android-ranlib")
+set(CMAKE_ASM_NASM_COMPILER_RANLIB "")
+set(CMAKE_LINKER "/Users/lambiengcode/Library/Android/sdk/ndk/21.1.6352462/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android-ld")
+set(CMAKE_ASM_NASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_NASM_COMPILER_ID "YASM")
+set(CMAKE_ASM_NASM_COMPILER_VERSION "")
+set(CMAKE_ASM_NASM_COMPILER_ENV_VAR "ASM_NASM")
+
+
+set(CMAKE_ASM_NASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_NASM_LINKER_PREFERENCE 0)
+
+
